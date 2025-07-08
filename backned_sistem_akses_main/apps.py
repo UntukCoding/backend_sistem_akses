14a062c73cef9b7d40b7801eb778df228d076222
fahmi-lenovo-arch <bravobintang24@gmail.com>
2025-07-08 12:08:53 +0700

    first commit

---
+++ backned_sistem_akses_main/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class BacknedSistemAksesMainConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'backned_sistem_akses_main'
